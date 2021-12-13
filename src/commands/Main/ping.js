a0163f639ad93a9ebe31bb72cab80064e8802168
Jack Meng <error666jm@gmail.com>
2021-12-12 22:19:02 -0500

    `legacy` ping command

---
+++ src/commands/Main/ping.js
@@ -0,0 +1,57 @@
+// legacy code
+// @ts-ignore
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+// @ts-ignore
+const config = require("../../../configs/token.json");
+// @ts-ignore
+const resource = require("../../../configs/resource.json");
+const { Database } = require("secure-db");
+const moment = require("moment");
+const fs = require("fs");
+module.exports = {
+  config: {
+    name: `ping`,
+    category: "",
+    description: "",
+    aliases: [`botping`],
+  },
+  // @ts-ignore
+  run: async (bot, message, args) => {
+    try {
+      message.channel.send(":ping_pong: Pinging...").then((m) => {
+        var ping = m.createdTimestamp - message.createdTimestamp;
+        var clientPing = Math.round(bot.pi);
+
+        m.edit(
+          `>>> **:ping_pong: Pong!** \n Bot Ping: ${ping}ms\nClient Ping: ${clientPing}ms`
+        );
+      });
+    } catch (err) {
+      console.error(err);
+      const embed = new MessageEmbed()
+        .setTitle("Whoops, looks like something went wrong!")
+        .setThumbnail(resource.aw_snap)
+        .setDescription(
+          "Use `" +
+            config.prefix +
+            "help` for a list of avaliable commands or use `" +
+            config.prefix +
+            "support` to join the support server!"
+        )
+        .setFooter("Still facing issues? Join the support server!");
+      fs.writeFile(
+        `${__dirname}/../../../logs/${Date.now()}_error.log`,
+        err + "\n",
+        function (err2) {
+          if (err2) {
+            return console.log(err2);
+          }
+        }
+      );
+
+      message.channel.send({ embeds: [embed] }).then((m) => {
+        m.delete({ timeout: 5000 });
+      });
+    }
+  },
+};
