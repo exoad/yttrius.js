389260a29c38a67fb63722c6ec683adf1f953792
Jack Meng <error666jm@gmail.com>
2021-12-13 20:31:24 -0500

    `legacy` serverinfo command

---
+++ src/commands/info/server-info.js
@@ -0,0 +1,97 @@
+// @ts-ignore
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+// @ts-ignore
+const config = require("../../../configs/token.json");
+// @ts-ignore
+const resource = require("../../../configs/resource.json");
+const { Database } = require("secure-db");
+const moment = require("moment");
+const fs = require("fs");
+module.exports = {
+  config: {
+    name: `serverinfo`,
+    category: "",
+    description: "",
+    aliases: [`server`],
+  },
+  // @ts-ignore
+  run: async (bot, message, args) => {
+    try {
+      if (!message.guild) {
+        message.reply("This command can only be used in a guild.");
+      }
+      const d = new Date(message.guild.createdTimestamp);
+      let day;
+      let month;
+      if (d.getDate() < 10) {
+        day = `0${d.getDate()}`;
+      } else {
+        day = d.getDate();
+      }
+      if (d.getMonth() + 1 < 10) {
+        month = `0${d.getMonth() + 1}`;
+      } else {
+        month = d.getMonth();
+      }
+
+      const guild = message.guild;
+      const members = message.guild.members.cache.array();
+      const Embed = new MessageEmbed()
+        .setTitle(`Guild name: ` + message.guild.name)
+        .setThumbnail(message.guild.iconURL())
+        .addFields([
+          { name: "Guild-Owner:", value: `<@${guild.ownerID}>`, inline: true },
+          { name: "Server-Region:", value: guild.region, inline: true },
+          { name: "Member Count:", value: guild.memberCount, inline: true },
+          {
+            name: "Verification Level:",
+            value: message.guild.verificationLevel,
+            inline: true,
+          },
+          {
+            name: "Channel Count:",
+            value: message.guild.channels.cache.size,
+            inline: true,
+          },
+          {
+            name: "Roles:",
+            value: message.guild.roles.cache.size,
+            inline: true,
+          },
+        ])
+        .setFooter(
+          `Server ID: ${
+            guild.id
+          } | Server Created: ${day}/${month}/${d.getFullYear()}`
+        );
+
+      message.channel.send({ embeds: [Embed] });
+    } catch (err) {
+      console.error(err);
+      const embed = new MessageEmbed()
+        .setTitle("Whoops, looks like something went wrong!")
+        .setThumbnail(resource.aw_snap)
+        .setDescription(
+          "Use `" +
+            config.prefix +
+            "help` for a list of avaliable commands or use `" +
+            config.prefix +
+            "support` to join the support server!"
+        )
+        .setFooter("Still facing issues? Join the support server!");
+      fs.writeFile(
+        `${__dirname}/../../../logs/${Date.now()}_error.log`,
+        err + "\n",
+        function (err2) {
+          if (err2) {
+            return console.log(err2);
+          }
+        }
+      );
+
+      message.channel.send({ embeds: [embed] }).then((m) => {
+        m.delete({ timeout: 5000 });
+      });
+    }
+  },
+};
